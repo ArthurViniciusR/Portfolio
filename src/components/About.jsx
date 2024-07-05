1d750cf60091aa2f694220224672d2432baeea4d
ArthurViniciusR <dev.artthur@gmail.com>
2024-07-05 20:21:57 -0300

    att-about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ function About() {
         <section id="about" className="pt-10">
             <h2 className="text-center dark:text-white text-xl font-bold my-10">Sobre</h2>
             <div className="w-3/4 max-w-screen-xl p-14 mx-auto rounded-md bg-primary-light dark:bg-secondary-dark" data-aos="fade-up">
-                <p className="dark:text-white text-sm">Meu nome é Arthur e sou programador, gosto muito de desenvolver, buscar desafios que façam eu melhorar meu conhecimento e busco constantemente aprender coisas novas para ser um desenvolverdor melhor, além disso, sou graduando em Sistemas para Internet, faço parte da liderança de uma comunidade de tecnologia e tenho experiência com startups</p>
+                <p className="dark:text-white text-sm">Sou um Desenvolvedor Web que está sempre em busca de desafios que façam eu melhorar meu conhecimento pessoal e profissional, buscando constantemente aprender coisas novas para ser um desenvolverdor melhor, além disso, sou graduando em Sistemas para Internet, faço parte da liderança de uma comunidade de tecnologia e tenho experiência com startups</p>
             </div>
         </section>
 
